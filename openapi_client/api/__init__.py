191769e50202c11f1dded60a99d772f570222ec2
Paul Bruce <me@paulsbruce.io>
2019-12-16 22:35:45 -0500

    Update API client based on preprod OpenAPI spec; provisional support for interrogating zones and reservations

--- openapi_client/api/__init__.py
+++ openapi_client/api/__init__.py
@@ -3,5 +3,6 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
+from openapi_client.api.resources_api import ResourcesApi
 from openapi_client.api.results_api import ResultsApi
 from openapi_client.api.runtime_api import RuntimeApi
