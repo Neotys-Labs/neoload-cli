97ae22accf01b07c67dc1d8d7e3b188fed7e80a5
Paul Bruce <8961042+paulsbruce@users.noreply.github.com>
2020-01-13 08:41:43 -0500

    Process improvements (#51)
    * Remove extraneous ls in Jenkinsfile
    
    * Increase Pypi wait time; remove docker pre-packaging block

--- upload.sh
+++ upload.sh
@@ -5,26 +5,6 @@ version_id=$(cat ./setup.py | grep -oE "(?:version=')(.*?)(?:')")
 version_id=$(echo "${version_id/version=/}")
 version_id=$(echo "${version_id//\'/}")
 
-docker build --build-arg PYPI_VERSION=$version_id -t neoload-cli --file resources/docker-neoload-cli/Dockerfile .
-if [ "$?" -ne "0" ]; then
-  echo "Docker-minimalist build failed"
-  exit 6
-fi
-docker run --rm neoload-cli neoload --version
-if [ "$?" -ne "0" ]; then
-  echo "Docker-minimalist image failed"
-  exit 7
-fi
-docker tag neoload-cli paulsbruce/neoload-cli:$version_id
-docker push paulsbruce/neoload-cli:$version_id
-if [ "$?" -ne "0" ]; then
-  echo "Docker-minimalist push failed"
-  exit 8
-fi
-docker rmi neoload-cli
-
-exit 5500
-
 if [ -z "$version_id" ]; then
     echo "No version found"
     exit 1
@@ -59,7 +39,7 @@ fi
 
 #TODO: run this script with a new version number. afterwards, run Azure job and ensure JSON schema validation
 echo "Waiting for < 1m after publishing to PyPi"
-sleep 60 # allow for some time on pypi
+sleep 300 # allow for some time on pypi
 
 docker build --build-arg PYPI_VERSION=$version_id -t neoload-cli --file resources/docker-neoload-cli/Dockerfile .
 if [ "$?" -ne "0" ]; then
