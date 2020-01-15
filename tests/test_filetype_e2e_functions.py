e57165563d318fcfff8bd2b3a39aadd3646bb3e7
Paul Bruce <me@paulsbruce.io>
2020-01-15 18:00:35 -0500

    Add tests for zip format (YAML only); classic .nlp tested locally

---
+++ tests/test_filetype_e2e_functions.py
@@ -0,0 +1,27 @@
+import pytest
+from utils import *
+from os import path
+
+################################################################################
+### Test Goal: verify that zip file is parsable and produces expected results
+################################################################################
+@pytest.mark.slow
+def test_attach_run_allinone():
+    requireTestSecrets()
+    assertProfileByZone(os.environ['NEOLOAD_CLI_ZONE_STATIC'])
+    assertNoDockerContainersRunning()
+
+    assertOutput(
+        contains=[
+            "Project uploaded",
+            "Test running",
+            "Test completed"
+        ],
+        printOutput=True,
+        clearConfig=False,
+        args={
+            '--debug': None,
+            '-f '+ os.path.abspath("tests/example_2_0_runtime/Archive.zip") : None,
+            '--scenario': 'sanityScenario',
+            '--attach': 'docker#1,neotys/neoload-loadgenerator:7.0.2'
+        })
