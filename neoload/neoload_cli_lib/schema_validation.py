f848c8171a7127fd63877814b9da21010359ca71
Guillaume BERT <guillaume.bert@neotys.com>
2020-04-07 12:34:26 +0200

    Store the schema file for yaml validation

--- neoload/neoload_cli_lib/schema_validation.py
+++ neoload/neoload_cli_lib/schema_validation.py
@@ -4,6 +4,7 @@ import requests
 import yaml
 from json import JSONDecodeError
 from yaml.scanner import ScannerError
+from neoload_cli_lib.user_data import update_schema, get_yaml_schema
 
 
 def validate_yaml(yaml_file_path, schema_url):
@@ -18,14 +19,19 @@ def validate_yaml(yaml_file_path, schema_url):
         raise Exception('This is not a valid yaml file :\n%s' % str(err))
 
     if schema_url is None:
-        # TODO get it from storage
-        pass
+        try:
+            json_schema = get_yaml_schema()
+        except Exception as err:
+            raise Exception('Error getting the schema from disk : %s' % str(err))
     else:
         try:
             json_schema = requests.get(schema_url).text
-            # TODO store it
         except Exception as err:
             raise Exception('Error getting the schema from the url: %s\n%s' % (schema_url, str(err)))
+        try:
+            update_schema(json_schema)
+        except Exception as err:
+            raise Exception('Error storing the schema : %s' % str(err))
 
     try:
         schema_as_object = json.loads(json_schema)
--- neoload/neoload_cli_lib/user_data.py
+++ neoload/neoload_cli_lib/user_data.py
@@ -7,6 +7,7 @@ __version = "1.0"
 __author = "neotys"
 __config_dir = appdirs.user_data_dir(__conf_name, __author, __version)
 __config_file = os.path.join(__config_dir, "config.yaml")
+__yaml_schema_file = os.path.join(__config_dir, "yaml_schema.json")
 
 __no_write = False
 
@@ -30,7 +31,7 @@ def do_login(token, url, no_write):
         raise Exception('token is mandatory. please see neoload login --help.')
     global __user_data_singleton
     __user_data_singleton = UserData.from_login(token, url)
-    __save()
+    __save_user_data()
     return __user_data_singleton
 
 
@@ -68,7 +69,7 @@ class UserData:
         return self.token
 
 
-def __load():
+def __load_user_data():
     if os.path.exists(__config_file):
         with open(__config_file, "r") as stream:
             load = yaml.load(stream, Loader=yaml.BaseLoader)
@@ -77,10 +78,10 @@ def __load():
     return None
 
 
-__user_data_singleton = __load()
+__user_data_singleton = __load_user_data()
 
 
-def __save():
+def __save_user_data():
     if not __no_write:
         os.makedirs(__config_dir, exist_ok=True)
         with open(__config_file, "w") as stream:
@@ -89,8 +90,32 @@ def __save():
 
 def set_meta(key, value):
     get_user_data().metadata[key] = value
-    __save()
+    __save_user_data()
 
 
 def get_meta(key):
     return get_user_data().metadata.get(key, None)
+
+
+def __load_yaml_schema():
+    if os.path.exists(__yaml_schema_file):
+        with open(__yaml_schema_file, "r") as stream:
+            return stream.read()
+    return None
+
+
+__yaml_schema_singleton = __load_yaml_schema()
+
+
+def get_yaml_schema(throw=True):
+    if __yaml_schema_singleton is None and throw:
+        raise Exception("No yaml schema found. Please add --schema-url option to download it first")
+    return __yaml_schema_singleton
+
+
+def update_schema(yaml_schema_as_json: str):
+    global __yaml_schema_singleton
+    __yaml_schema_singleton = yaml_schema_as_json
+    os.makedirs(__config_dir, exist_ok=True)
+    with open(__yaml_schema_file, "w") as stream:
+        stream.write(__yaml_schema_singleton)
