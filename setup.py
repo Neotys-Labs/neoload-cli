8679cf77e1b69ea21d1bf697257ca9c42c3ff248
Stephane Martin <stephane.martin@neotys.com>
2020-04-21 09:22:04 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup, find_packages
 
+from os import path
+from io import open
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='neoload-cli',
-    version='1.0.0',
+    version='1.0.0.rc1',
+    package_data={'': [
+        'LICENSE',
+        'README.md'
+    ]},
     packages=find_packages(exclude=("tests",)),
     entry_points={
         'console_scripts': [
@@ -26,5 +37,7 @@ setup(
         'junit_xml',
         'termcolor',
         'coloredlogs'
-    ]
+    ],
+    long_description_content_type='text/markdown',
+    long_description=long_description
 )
