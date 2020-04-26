f9652251ec9d87a7800b52100dd4e20951027291
Paul Bruce <me@paulsbruce.io>
2020-04-26 17:04:44 -0400

    Update to handle improper attach, bad zone coverage

--- tests/commands/docker/test_attach.py
+++ tests/commands/docker/test_attach.py
@@ -4,6 +4,7 @@ from docker_test_utils import *
 
 @pytest.mark.docker
 @pytest.mark.integration
+@pytest.mark.slow
 @pytest.mark.usefixtures("neoload_login")  # it's like @Before on the neoload_login function
 class TestAttach:
     def test_minimal(self, monkeypatch):
@@ -11,7 +12,7 @@ class TestAttach:
 
         def validate(context, result):
             assert_success(result)
-            assert 'All containers are attached and ready for use' in result.output
+            assert 'All containers are attached' in result.output
 
         setup_lifecycle(runner,
             between_create_delete=lambda context: attach_detach_lifecycle(context,
