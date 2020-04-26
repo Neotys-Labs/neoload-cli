8c0125060b14c61bff225023c015f1c944b80f6b
Paul Bruce <me@paulsbruce.io>
2020-04-26 17:06:00 -0400

    Update to handle no prior containers; coverage

--- tests/commands/docker/test_detach.py
+++ tests/commands/docker/test_detach.py
@@ -2,8 +2,11 @@ import pytest
 from click.testing import CliRunner
 from docker_test_utils import *
 
+from commands.docker import cli as docker
+
 @pytest.mark.docker
 @pytest.mark.integration
+@pytest.mark.slow
 @pytest.mark.usefixtures("neoload_login")  # it's like @Before on the neoload_login function
 class TestDetach:
     def test_minimal(self, monkeypatch):
@@ -11,10 +14,21 @@ class TestDetach:
 
         def validate(context, result):
             assert_success(result)
-            assert 'All containers and networks with' in result.output
+            print(result.output)
+
+            assert ('No containers or networks' in result.output or
+                    'All containers and networks with' in result.output or
+                    '0 artifacts' in result.output
+            )
 
         setup_lifecycle(runner,
             between_create_delete=lambda context: attach_detach_lifecycle(context,
                 after_detach=validate
             )
         )
+
+    def test_force_all_detach(self, monkeypatch):
+        runner = CliRunner()
+
+        detach_result = runner.invoke(docker, ['--force', '--all', 'detach'])
+        assert_success(detach_result)
