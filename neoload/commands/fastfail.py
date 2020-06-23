f348c33d387760a327b758bc1adfaed7120271f7
Paul Bruce <me@paulsbruce.io>
2020-06-23 09:05:32 -0400

    Refactor loop to resolve code complexities

--- neoload/commands/fastfail.py
+++ neoload/commands/fastfail.py
@@ -32,10 +32,10 @@ def cli(command, name, stop, force, max_failure): #, max_occurs):
 
     else:
         tools.system_exit({'message': "Invalid command. Please see neoload fastfail --help", 'code': 2})
-        return
 
 def monitor_loop(name, stop, force, max_failure):
     dt_started = datetime.now()
+    print('fastfail started: ' + str(dt_started))
     dt_current = dt_started
 
     if sys.stdin.isatty():
@@ -43,6 +43,7 @@ def monitor_loop(name, stop, force, max_failure):
 
     is_initializing = False
     is_running = False
+    has_exited = False
     while (abs(dt_current-dt_started).seconds / 60) < 10:
         datas = test_results.get_sla_data_by_name_or_id(name)
 
@@ -54,43 +55,68 @@ def monitor_loop(name, stop, force, max_failure):
         fails.extend(list(filter(lambda x: x['status']=='FAILED',datas['sla_test'])))
         fails.extend(failed_intervals)
 
-        if len(fails) > 0 or len(partial_intervals) > 0:
-            print('\n\n')
-            displayer.__print_sla(datas['sla_global'], datas['sla_test'], datas['sla_interval'])
-            #print(yaml.dump(fails))
-
         status = datas['result']['status']
-        qualityStatus = datas['result']['qualityStatus']
+        quality_status = datas['result']['qualityStatus']
 
-        msg = "{0} hard SLA failures, test status {1},{2}".format(len(fails), status, qualityStatus)
+        msg = "{0} hard SLA failures, test status {1},{2}".format(len(fails), status, quality_status)
         if len(partial_intervals) > 0 and len(failed_intervals) < 1:
             msg += "\nSome SLAs failed, but were not above max-failure of {0}% so did not count as error.".format(max_failure)
 
+        outcomes = process_state(datas,fails,stop,force,status,is_initializing,is_running,msg)
 
-        if len(fails) > 0:
-            if stop and status != 'TERMINATED':
-                print("Stopping test, force={0}".format(force))
-                tools.set_batch(True)
-                running_tools.stop(datas['id'], force)
-            tools.system_exit({'message': msg, 'code': 1})
-            break
-        elif status == 'TERMINATED':
-            tools.system_exit({'message': msg, 'code': 0})
-            break
-        elif status == 'INIT':
-            if not is_initializing:
-                print('Initializing', end = '')
-            is_initializing = True
-        elif status == 'RUNNING':
-            if not is_running:
-                print('Running', end = '')
-            is_running = True
+        final_run = has_exited and outcomes["has_exited"]
+        has_exited = outcomes["has_exited"]
+        is_initializing = outcomes["is_initializing"]
+        is_running = outcomes["is_running"]
 
-        if sys.stdin.isatty():
-            print('.', end = '')
+        if not final_run and len(fails) > 0 or len(partial_intervals) > 0:
+            displayer.__print_sla(datas['sla_global'], datas['sla_test'], datas['sla_interval'])
+
+        if final_run:
+            break
+
+        printif(sys.stdin.isatty() and not has_exited, '.', end = '')
+
         time.sleep(5)
 
         dt_current = datetime.now()
+
+    print('fastfail ended: ' + str(dt_current))
+
+def process_state(datas,fails,stop,force,status,is_initializing,is_running,msg):
+    ret = {
+        "has_exited": False,
+        "is_initializing": is_initializing,
+        "is_running": is_running
+    }
+
+    if len(fails) > 0:
+        if status != 'TERMINATED':
+            if stop:
+                print("\nStopping test, force={0}".format(force))
+                tools.set_batch(True)
+                running_tools.stop(datas["id"], force)
+        else:
+            tools.system_exit({'message': msg, 'code': 1})
+        ret["has_exited"] = True
+    elif status == 'TERMINATED':
+        tools.system_exit({'message': msg, 'code': 0 if datas['result']['qualityStatus']=="PASSED" else 1})
+        ret["has_exited"] = True
+    elif status == 'INIT':
+        printif(not is_initializing, 'Initializing', end = '')
+        is_initializing = True
+    elif status == 'RUNNING':
+        printif(not is_running, '\nRunning', end = '')
+        is_running = True
+
+    ret["is_initializing"] = is_initializing
+    ret["is_running"] = is_running
+
+    return ret
+
+def printif(should_print, msg, end='\n'):
+    if should_print:
+        print(msg,end=end)
 
 class Unbuffered(object):
    def __init__(self, stream):
