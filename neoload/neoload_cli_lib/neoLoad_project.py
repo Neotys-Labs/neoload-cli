38cac71ec5aa8068ce7e6c7eb2f0530dcbb3daea
Paul Bruce <me@paulsbruce.io>
2020-08-12 14:00:42 -0400

    Add functionality to project upload to use .nlignore file

--- neoload/neoload_cli_lib/neoLoad_project.py
+++ neoload/neoload_cli_lib/neoLoad_project.py
@@ -2,6 +2,10 @@ import os
 import tempfile
 import zipfile
 
+import logging
+import os
+from gitignore_parser import parse_gitignore
+
 from neoload_cli_lib import rest_crud, tools, cli_exception
 
 black_list = ['recorded-requests/', 'recorded-responses/', 'recorded-screenshots/', '.git/', '.svn/', 'results/',
@@ -14,15 +18,30 @@ def is_black_listed(path: str):
             return True
     return False
 
+def is_nl_ignored(matcher, file_path):
+    if matcher is None: return False
+    return matcher(file_path)
 
 def zip_dir(path):
+    # find and load .nlignore
+    ignorefile = os.path.join(path,'.nlignore')
+    nlignore_matcher = None
+    if os.path.exists(ignorefile):
+        nlignore_matcher = parse_gitignore(ignorefile)
+
     temp_zip = tempfile.TemporaryFile('w+b')
     ziph = zipfile.ZipFile(temp_zip, 'x', zipfile.ZIP_DEFLATED)
     for root, dirs, files in os.walk(path):
         for file in files:
             file_path = os.path.join(root, file)
             if not is_black_listed(file_path):
-                ziph.write(file_path, file_path.replace(str(path), ''))
+                if not is_nl_ignored(nlignore_matcher, file_path):
+                    ziph.write(file_path, file_path.replace(str(path), ''))
+                else:
+                    logging.debug(".nlignore'd: '" + file_path + "'")
+            else:
+                logging.debug("blacklisted: '" + file_path + "'")
+
     ziph.close()
     temp_zip.seek(0)
     return temp_zip
