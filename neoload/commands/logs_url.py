7b97ef9a516b72f817bfef87283b16976b4576ee
Paul Bruce <me@paulsbruce.io>
2020-09-02 16:36:43 -0400

    Add 'cur' pseudonym support required by name argument

--- neoload/commands/logs_url.py
+++ neoload/commands/logs_url.py
@@ -5,11 +5,28 @@ import click
 from commands import test_results
 from neoload_cli_lib import user_data, tools
 
+from neoload_cli_lib.name_resolver import Resolver
+from neoload_cli_lib import rest_crud, user_data
+
+__endpoint = "/test-results"
+
+__resolver = Resolver(__endpoint, rest_crud.base_endpoint_with_workspace)
+
+meta_key = 'result id'
 
 @click.command()
 @click.argument('name', type=str)
 def cli(name):
     """get logs url of a test result managed by NeoLoad Web"""
+
+    if name == "cur":
+        name = user_data.get_meta(meta_key)
+    is_id = tools.is_id(name)
+
+    __id = tools.get_id(name, __resolver, is_id)
+
+    if not __id:
+        __id = user_data.get_meta_required(meta_key)
 
     print(get_url(name))
 
