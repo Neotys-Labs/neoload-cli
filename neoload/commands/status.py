83f53bf78cc53bf739f45bc2c6586605aa1fa7c5
Paul Bruce <me@paulsbruce.io>
2020-10-01 12:37:01 -0400

    Add status output of interactive when in debug mode

--- neoload/commands/status.py
+++ neoload/commands/status.py
@@ -1,7 +1,7 @@
 import click
 
-from neoload_cli_lib import user_data
-
+from neoload_cli_lib import user_data, tools
+import logging
 
 @click.command()
 def cli():
@@ -11,3 +11,9 @@ def cli():
         print("No settings is stored. Please use \"neoload login\" to start.")
     else:
         print(login)
+
+        environ = {}
+        environ["interactive_implied"] = tools.is_user_interactive_implied()
+        environ["interactive_environment_set"] = tools.get_user_interactive_value()
+        environ["interactive_effective"] = tools.is_user_interactive()
+        logging.debug(environ)
