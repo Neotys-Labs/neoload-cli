741bdfa73215013d62e527f931ec245f9c7b5b41
Guillaume BERT <guillaume.bert@neotys.com>
2020-10-29 17:22:27 +0100

    Add more unit tests

--- tests/commands/test_results/test_result_ls.py
+++ tests/commands/test_results/test_result_ls.py
@@ -41,6 +41,53 @@ class TestResultLs:
         json_result = json.loads(result_ls.output)
         assert len(json_result) == 1
 
+    def test_list_filter_unknown(self, monkeypatch, valid_data):
+        runner = CliRunner()
+        mock_api_get_with_pagination(monkeypatch, 'v2/test-results',
+                     '[{"id":"someId", "name":"test-name", "description":".... "},'
+                     '{"id":"someId", "name":"test-name-2", "description":".... "}]')
+        result_ls = runner.invoke(results, ['ls', '--filter','unknown=test-name-2'])
+        assert_success(result_ls)
+        json_result = json.loads(result_ls.output)
+        assert len(json_result) == 0
+
+    def test_list_filter_regex(self, monkeypatch, valid_data):
+        runner = CliRunner()
+        mock_api_get_with_pagination(monkeypatch, 'v2/test-results',
+                     '[{"id":"someId1", "name":"test-name", "description":".... "},'
+                     '{"id":"someId2", "name":"test-name-0", "description":"I\'m good !!"},'
+                     '{"id":"someId3", "name":"test-name-2", "description":".... "}]')
+        result_ls = runner.invoke(results, ['ls', '--filter','name=test-name-[0-9]'])
+        assert_success(result_ls)
+        json_result = json.loads(result_ls.output)
+        assert len(json_result) == 2
+        assert json_result[0]['id'] == "someId2"
+        assert json_result[1]['id'] == "someId3"
+
+    def test_list_filter_regex_wildcard(self, monkeypatch, valid_data):
+        runner = CliRunner()
+        mock_api_get_with_pagination(monkeypatch, 'v2/test-results',
+                     '[{"id":"someId1", "name":"test-name", "description":".... "},'
+                     '{"id":"someId2", "name":"test-name-2", "description":".... "}]')
+        result_ls = runner.invoke(results, ['ls', '--filter','name=test-name.*'])
+        assert_success(result_ls)
+        json_result = json.loads(result_ls.output)
+        assert len(json_result) == 2
+        assert json_result[0]['id'] == "someId1"
+        assert json_result[1]['id'] == "someId2"
+
+    def test_list_filter_multiple(self, monkeypatch, valid_data):
+        runner = CliRunner()
+        mock_api_get_with_pagination(monkeypatch, 'v2/test-results',
+                     '[{"id":"someId1", "name":"test-name", "description":".... "},'
+                     '{"id":"someId2", "name":"test-name-1", "description":"I\'m bad"},'
+                     '{"id":"someId3", "name":"test-name-2", "description":"I\'m good !!"}]')
+        result_ls = runner.invoke(results, ['ls', '--filter','name=test-name-[0-9];description=^I\'m good*'])
+        assert_success(result_ls)
+        json_result = json.loads(result_ls.output)
+        assert len(json_result) == 1
+        assert json_result[0]['id'] == "someId3"
+
     def test_error_not_logged_in(self):
         runner = CliRunner()
         result_logout = runner.invoke(logout)
