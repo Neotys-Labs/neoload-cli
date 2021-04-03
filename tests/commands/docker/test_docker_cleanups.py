4ab3fe810da64ed3949cef1ca5690b5606d190de
Paul Bruce <8961042+paulsbruce@users.noreply.github.com>
2021-04-03 11:36:41 -0400

    Add docker coverage (#147)
    * Add Docker tests and add to PR/MR workflow; fix no-containers bug
    
    * Update conditionals for integration testing

---
+++ tests/commands/docker/test_docker_cleanups.py
@@ -0,0 +1,24 @@
+import pytest
+import os
+from click.testing import CliRunner
+from commands.login import cli as login
+from commands.status import cli as status
+from commands.docker import cli as docker
+from neoload_cli_lib import docker_lib
+from helpers.test_utils import *
+import json
+import tempfile
+
+@pytest.mark.docker
+@pytest.mark.slow
+@pytest.mark.usefixtures("neoload_login")  # it's like @Before on the neoload_login function
+class TestDockerCleanups:
+    def test_docker_clean(self):
+        runner = CliRunner()
+        result = runner.invoke(docker, ['clean'])
+        assert_success(result)
+
+    def test_docker_forget(self):
+        runner = CliRunner()
+        result = runner.invoke(docker, ['forget'])
+        assert_success(result)
