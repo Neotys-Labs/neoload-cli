e31286db73020b4df4b9ddf91c1a8cce49e25dcd
Guillaume BERT <guillaume.bert@neotys.com>
2021-05-24 09:52:14 +0200

    LOAD-23975  [CLI Python] Ability to set external URL in run command

--- tests/integration/scripts/run.test.sh
+++ tests/integration/scripts/run.test.sh
@@ -49,12 +49,35 @@ assertJsonEquals '.scenarios[0].scenarioName' '"simpledemo"'
 
 
 # Run the test
-cmd='python neoload run --scenario simpledemo'
+cmd='python neoload run --scenario simpledemo --external-url "https://jenkins/1234" --external-url-label "Jenkins build 1234"'
 out=`eval $cmd > run.log`
 assertEquals "$?" '0'
 echo "[DONE]" $cmd "Write output to file run.log"
 echo ">>> You MUST check manually that the differences below are ONLY IDs and numbers !!"
 diff run.log tests/integration/expected/run.log
 rm -f run.log
+
+# Check the result
+cmd='python neoload test-results ls "CLI-1"'
+out=`eval $cmd`
+assertEquals "$?" '0'
+resultId=`echo $out | jq .id`
+assertJsonEquals '.name' '"CLI-1"'
+assertJsonEquals '.description' '""'
+assertJsonEquals '.qualityStatus' '"PASSED"'
+assertJsonEquals '.externalUrl' '"https://jenkins/1234"'
+assertJsonEquals '.externalUrlLabel' '"Jenkins build 1234"'
+
+# Patch with empty external URL to go back to the initial state
+cmd='python neoload test-results --external-url "" --external-url-label "" patch'
+out=`eval $cmd`
+assertEquals "$?" '0'
+assertJsonEquals '.id' "${resultId}"
+assertJsonEquals '.name' '"CLI-1"'
+assertJsonEquals '.description' '""'
+assertJsonEquals '.qualityStatus' '"PASSED"'
+assertJsonEquals '.externalUrl' '""'
+assertJsonEquals '.externalUrlLabel' '""'
+
 
 exit $fails
