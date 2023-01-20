b726e4983f161fc8354aad122478da4a15680b2f
Guillaume BERT <57721694+guillaumebert@users.noreply.github.com>
2023-01-20 10:21:33 +0100

    Add missing commands

--- neoload/commands/__init__.py
+++ neoload/commands/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ["login", "logout", "logs_url", "project", "run", "status", "stop", "test_results", "test_settings",
-           "validate", "wait", "zones", "report"]
+__all__ = ["config", "docker", "fastfail", "login", "logout", "logs_url", "project", "run", "status", "stop", "test_results", "test_settings",
+           "validate", "wait", "workspaces", "zones", "report"]
