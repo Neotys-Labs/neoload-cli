709c4eb6dc2b382442cc04d94eb289a165086caa
Guyottot Gaetan <gguyottot.ggaeta@gmail.com>
2024-06-12 11:53:36 +0200

    Added a password verification and request on NLP projects.
    # Conflicts:
    #	neoload/commands/logs.py
    #	neoload/neoload_cli_lib/logs_tools.py
    #	neoload/neoload_cli_lib/running_tools.py

--- neoload/commands/project.py
+++ neoload/commands/project.py
@@ -1,15 +1,16 @@
 import os
-
+import zipfile
 import click
-
+import urllib.parse as urlparse
 from commands import test_settings
+from pathlib import Path
 from neoload_cli_lib import user_data, tools, rest_crud, neoLoad_project
 
 
 @click.command()
 @click.argument("command", required=True, type=click.Choice(['up', 'upload', 'meta']))
 @click.option("--path", "-p", type=click.Path(exists=True), default=os.getcwd(),
-              help="path of project folder, zip or yml file. . is default value")
+              help="Path of project folder, zip or yml file. . is default value")
 @click.option("--save", "-s", type=click.Path(exists=False),
               help="Path to a (non-existent) file ending in .zip to preserve what was uploaded")
 @click.argument("name_or_id", type=str, required=False)
@@ -18,10 +19,8 @@ def cli(command, name_or_id, path, save):
     rest_crud.set_current_command()
     if not name_or_id or name_or_id == "cur":
         name_or_id = user_data.get_meta(test_settings.meta_key)
-
     if not tools.is_id(name_or_id):
         name_or_id = test_settings.__resolver.resolve_name(name_or_id)
-
     if command[:2] == "up":
         upload(path, name_or_id, save)
     elif command == "meta":
@@ -30,12 +29,55 @@ def cli(command, name_or_id, path, save):
     rest_crud.set_current_sub_command(command)
 
 
+def extract_nlp_from_zip(zip_path):
+    extract_path = zip_path.parent
+    with zipfile.ZipFile(zip_path, 'r') as zipf:
+        for file_name in zipf.namelist():
+            if file_name.endswith('.nlp'):
+                zipf.extract(file_name, extract_path)
+                return Path(extract_path / file_name)
+    return None
+
+
+def find_password_in_nlp(nlp_file_path):
+    with open(nlp_file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            if 'project.password.hash=' in line:
+                return line.split('project.password.hash=')[1].strip()
+    return None
+
+
 #TODO: pre-validate with 'neoload validate' functionality, but..
 #TODO: provide a --skip-validation option
 #TODO: spider through all YAML (as-code files)
 #TODO: fix validate to recurse through all includes; create unique file list map (avoid recursive references)
 
 def upload(path, settings_id, save):
+    path = Path(path)
+    nlp_file_path = None
+
+    if path.suffix == '.zip':
+        nlp_file_path = extract_nlp_from_zip(path)
+    else:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith(".nlp"):
+                    nlp_file_path = Path(root) / file
+                    break
+            if nlp_file_path:
+                break
+
+    password = None
+    if nlp_file_path and nlp_file_path.exists():
+        password = find_password_in_nlp(nlp_file_path)
+        os.remove(nlp_file_path)
+
+    if password:
+        print(f"Password found: {password}")
+        print("Your project has a password, please enter your password here: " +
+              urlparse.urljoin(user_data.get_user_data().get_frontend_url(), "/#!test-settings/" + settings_id))
+
+    # Always call the upload_project method
     neoLoad_project.upload_project(path, get_endpoint(settings_id), save)
 
 
